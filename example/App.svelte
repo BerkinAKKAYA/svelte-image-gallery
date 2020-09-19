df3013c5f7a8444e69f836cf4e25065750ac491d
Berkin AKKAYA <berkin_akkaya@hotmail.com>
2020-09-19 14:21:12 +0300

    Example Updated

--- example/App.svelte
+++ example/App.svelte
@@ -5,18 +5,18 @@
 <Gallery gap={15} maxColumnWidth={250}>
 	<img src="https://via.placeholder.com/210x170/100" alt="">
 	<img src="https://via.placeholder.com/180x200/100" alt="">
-	<img src="https://via.placeholder.com/200x480/100" alt="">
-	<img src="https://via.placeholder.com/140x810/100" alt="">
+	<img src="https://via.placeholder.com/200x210/100" alt="">
+	<img src="https://via.placeholder.com/140x250/100" alt="">
 	<img src="https://via.placeholder.com/250x300/100" alt="">
-	<img src="https://via.placeholder.com/280x400/100" alt="">
-	<img src="https://via.placeholder.com/220x100/100" alt="">
-	<img src="https://via.placeholder.com/180x140/100" alt="">
+	<img src="https://via.placeholder.com/280x200/100" alt="">
+	<img src="https://via.placeholder.com/220x180/100" alt="">
+	<img src="https://via.placeholder.com/180x150/100" alt="">
 	<img src="https://via.placeholder.com/210x210/100" alt="">
 	<img src="https://via.placeholder.com/200x200/100" alt="">
 	<img src="https://via.placeholder.com/220x200/100" alt="">
-	<img src="https://via.placeholder.com/180x740/100" alt="">
+	<img src="https://via.placeholder.com/180x310/100" alt="">
 	<img src="https://via.placeholder.com/210x210/100" alt="">
-	<img src="https://via.placeholder.com/200x520/100" alt="">
-	<img src="https://via.placeholder.com/210x470/100" alt="">
-	<img src="https://via.placeholder.com/180x500/100" alt="">
+	<img src="https://via.placeholder.com/200x280/100" alt="">
+	<img src="https://via.placeholder.com/210x350/100" alt="">
+	<img src="https://via.placeholder.com/180x270/100" alt="">
 </Gallery>
